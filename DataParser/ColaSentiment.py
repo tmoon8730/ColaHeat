cf10e23465d1e8f8d5d4d72c482a45036dd85ceb
Tyler Moon <moon.tyler@gmail.com>
2018-04-14 12:01:48 -0400

    Added geocode to query

--- DataParser/ColaSentiment.py
+++ DataParser/ColaSentiment.py
@@ -63,7 +63,7 @@ class TwitterClient(object):
 
         try:
             # Call the api to get the tweets
-            fetched_tweets = self.api.search(q = query, count = count)
+            fetched_tweets = self.api.search(q = query, count = count, geocode="34.000471,-81.041786,50mi")
 
             # Parse each tweet
             for tweet in fetched_tweets:
@@ -87,19 +87,23 @@ class TwitterClient(object):
 
 def main():
     api = TwitterClient()
-    tweets = api.get_tweets(query = 'Donald Trump', count = 200)
+    tweets = api.get_tweets(query = ' ', count = 200)
+
+    # Parse out the positive and negative tweets
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
-    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
     ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
-    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
-    print("Neutral tweets percentage: {} % \
-        ".format(100*(len(tweets) - len(ntweets) - len(ptweets))/len(tweets)))
+
     print("\n\nPositive tweets:")
     for tweet in ptweets[:10]:
         print(tweet['text'])
     print("\n\nNegative tweets:")
     for tweet in ntweets[:10]:
         print(tweet['text'])
+
+
+    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+
 
 
 if __name__ == "__main__":
