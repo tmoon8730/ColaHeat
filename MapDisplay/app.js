ac8870eef841791e98259468e23290421d27adec
tbhall <tbhall@email.sc.edu>
2018-04-14 11:33:34 -0400

    added basic index.html

--- MapDisplay/app.js
+++ MapDisplay/app.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
+// load the things we need
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+});
+
+app.listen(3000);
+console.log('3000 is the magic port');
